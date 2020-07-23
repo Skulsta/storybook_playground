741432d5e14e85f844b6468367ba6b5acf6634ce
skulsta <sti.sku@gmail.com>
2020-07-23 11:55:03 +0200

    change task background to red

--- src/components/Task.js
+++ src/components/Task.js
@@ -19,9 +19,9 @@ const task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
           value={title}
           readOnly={true}
           placeholder="Input title"
+          style={{ textOverflow: "ellipsis", background: "red" }}
         />
       </div>
-
       <div className="actions" onClick={(event) => event.stopPropagation()}>
         {state !== "TASK_ARCHIVED" && (
           // eslint-disable-next-line jsx-a11y/anchor-is-valid
